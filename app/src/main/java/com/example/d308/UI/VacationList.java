13810ac6aa280087b9f89420b9b6dedfb8163135
Gary Smith <garysmith193358@gmail.com>
2023-09-18 21:42:27 -0400

    added naviagation to vacation list

--- app/src/main/java/com/example/d308/UI/VacationList.java
+++ app/src/main/java/com/example/d308/UI/VacationList.java
@@ -2,9 +2,12 @@ package com.example.d308.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.d308.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class VacationList extends AppCompatActivity {
 
@@ -12,6 +15,14 @@ public class VacationList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vacation_list);
-        System.out.println(getIntent().getStringExtra("test"));
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(VacationList.this, VacationDetails.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
