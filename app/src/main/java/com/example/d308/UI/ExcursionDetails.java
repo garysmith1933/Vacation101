d242ee9c908e6b3e5fbdd20568908ec41ab28e52
Gary Smith <garysmith193358@gmail.com>
2023-09-18 23:10:34 -0400

    added excursion details page

---
+++ app/src/main/java/com/example/d308/UI/ExcursionDetails.java
@@ -0,0 +1,16 @@
+package com.example.d308.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.d308.R;
+
+public class ExcursionDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_excursion_details);
+    }
+}
--- app/src/main/java/com/example/d308/UI/VacationDetails.java
+++ app/src/main/java/com/example/d308/UI/VacationDetails.java
@@ -2,9 +2,12 @@ package com.example.d308.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.d308.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class VacationDetails extends AppCompatActivity {
 
@@ -12,5 +15,13 @@ public class VacationDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vacation_details);
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton2);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(VacationDetails.this, ExcursionDetails.class);
+                startActivity(intent);
+            }
+        });
     }
 }
