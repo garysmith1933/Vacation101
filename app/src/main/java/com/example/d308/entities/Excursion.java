838259187ff9cb1aa15f6710f7fd5c562d822091
Gary Smith <garysmith193358@gmail.com>
2023-09-19 00:45:21 -0400

    created excursion entity, including constructor, getters, and setters

---
+++ app/src/main/java/com/example/d308/entities/Excursion.java
@@ -0,0 +1,56 @@
+package com.example.d308.entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity(tableName = "excursions")
+public class Excursion {
+    @PrimaryKey(autoGenerate = true)
+    private int excursionID;
+    private String excursionName;
+
+    private Date startDate;
+
+    public Excursion(int excursionID, String excursionName, Date startDate, int vacationID) {
+        this.excursionID = excursionID;
+        this.excursionName = excursionName;
+        this.startDate = startDate;
+        this.vacationID = vacationID;
+    }
+
+    public int getExcursionID() {
+        return excursionID;
+    }
+
+    public void setExcursionID(int excursionID) {
+        this.excursionID = excursionID;
+    }
+
+    public String getExcursionName() {
+        return excursionName;
+    }
+
+    public void setExcursionName(String excursionName) {
+        this.excursionName = excursionName;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getVacationID() {
+        return vacationID;
+    }
+
+    public void setVacationID(int vacationID) {
+        this.vacationID = vacationID;
+    }
+
+    private int vacationID;
+}
