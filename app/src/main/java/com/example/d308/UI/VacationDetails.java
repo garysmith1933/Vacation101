c2c7215365af9602a02043b0a1b662c8191fd09d
Gary Smith <garysmith193358@gmail.com>
2023-09-19 22:52:34 -0400

    added calendar views to front end to pick dates

--- app/src/main/java/com/example/d308/UI/VacationDetails.java
+++ app/src/main/java/com/example/d308/UI/VacationDetails.java
@@ -6,20 +6,49 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.example.d308.R;
 import com.example.d308.database.Repository;
+import com.example.d308.entities.Vacation;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class VacationDetails extends AppCompatActivity {
+    String name;
+    String hotel;
+    int vacationID;
+    EditText editName;
+    EditText editHotelName;
 
+    CalendarView editStartDate;
+
+    CalendarView editEndDate;
+
+    Repository repository;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Repository repository;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vacation_details);
         FloatingActionButton fab = findViewById(R.id.floatingActionButton2);
+        editName=findViewById(R.id.titletext);
+        editHotelName=findViewById(R.id.hoteltext);
+        editStartDate = findViewById(R.id.startDate);
+        editEndDate = findViewById(R.id.endDate);
+
+        name = getIntent().getStringExtra("name");
+        editName.setText(name);
+
+        hotel = getIntent().getStringExtra("hotel");
+        editHotelName.setText(hotel);
+
+        vacationID = getIntent().getIntExtra("id", -1);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,5 +62,23 @@ public class VacationDetails extends AppCompatActivity {
         recyclerView.setAdapter(excursionAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         excursionAdapter.setExcursions(repository.getmAllExcursions());
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_vacation_details, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.vacationSave ) {
+            Vacation vacation;
+            if (vacationID == -1) {
+                if (repository.getmAllVacations().size() == 0) vacationID = 1;
+                else vacationID = repository.getmAllVacations().get(repository.getmAllVacations().size()-1).getVacationID()+1;
+//                vacation = new Vacation(vacationID, editName.getText().toString(), editHotelName.getText().toString()));
+            }
+        }
+
+        return true;
     }
 }
