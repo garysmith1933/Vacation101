8e13a2ef4c5769134c114cf4aa7107c7425e2d12
Gary Smith <garysmith193358@gmail.com>
2023-09-19 21:58:39 -0400

    excurisons are able to be displayed

---
+++ app/src/main/java/com/example/d308/UI/ExcursionAdapter.java
@@ -0,0 +1,89 @@
+package com.example.d308.UI;
+
+import android.content.Context;
+import android.content.Intent;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.d308.R;
+import com.example.d308.entities.Excursion;
+import com.example.d308.entities.Vacation;
+
+import java.util.List;
+
+public class ExcursionAdapter extends RecyclerView.Adapter<ExcursionAdapter.ExcursionViewHolder> {
+    private List<Excursion> mExcursions;
+    private final Context context;
+
+    private final LayoutInflater mInflater;
+
+    class ExcursionViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView excurisonItemView;
+        private final TextView excurisonItemView2;
+
+
+        public ExcursionViewHolder(@NonNull View itemView) {
+            super(itemView);
+            excurisonItemView = itemView.findViewById(R.id.textView3);
+            excurisonItemView2 = itemView.findViewById(R.id.textView4);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    final Excursion current = mExcursions.get(position);
+                    Intent intent = new Intent(context, ExcursionDetails.class);
+                    intent.putExtra("id", current.getExcursionID());
+                    intent.putExtra("name", current.getExcursionName());
+                    context.startActivity(intent);
+                }
+            });
+        }
+    }
+    public ExcursionAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+
+    @Override
+    public ExcursionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.excursion_list_item, parent, false);
+        return new ExcursionViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExcursionViewHolder holder, int position) {
+        if (mExcursions != null) {
+            Excursion current = mExcursions.get(position);
+            String name = current.getExcursionName();
+            int excursionID = current.getExcursionID();
+            holder.excurisonItemView.setText(name);
+            holder.excurisonItemView2.setText(Integer.toString(excursionID));
+        }
+
+        else {
+            holder.excurisonItemView.setText("No excursion name");
+            holder.excurisonItemView2.setText("No excursion id");
+        }
+    }
+
+    public void setExcursions(List<Excursion> excursions) {
+        mExcursions = excursions;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mExcursions != null) {
+            return mExcursions.size();
+        }
+
+        return 0;
+    }
+}
--- app/src/main/java/com/example/d308/UI/VacationDetails.java
+++ app/src/main/java/com/example/d308/UI/VacationDetails.java
@@ -1,18 +1,22 @@
 package com.example.d308.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.d308.R;
+import com.example.d308.database.Repository;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class VacationDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Repository repository;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vacation_details);
         FloatingActionButton fab = findViewById(R.id.floatingActionButton2);
@@ -23,5 +27,11 @@ public class VacationDetails extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        RecyclerView recyclerView = findViewById(R.id.excursionRecyclerView);
+        repository = new Repository((getApplication()));
+        final ExcursionAdapter excursionAdapter = new ExcursionAdapter(this);
+        recyclerView.setAdapter(excursionAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        excursionAdapter.setExcursions(repository.getmAllExcursions());
     }
 }
