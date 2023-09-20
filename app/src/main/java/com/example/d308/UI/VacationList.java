567ecdb8659f230cd0abee80f4ac4f104dd85742
Gary Smith <garysmith193358@gmail.com>
2023-09-19 21:23:48 -0400

    data can be shown on the frontend

--- app/src/main/java/com/example/d308/UI/VacationList.java
+++ app/src/main/java/com/example/d308/UI/VacationList.java
@@ -1,6 +1,8 @@
 package com.example.d308.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,6 +18,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class VacationList extends AppCompatActivity {
     private Repository repository;
@@ -31,10 +34,19 @@ public class VacationList extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        repository = new Repository(getApplication());
+        List<Vacation> allVacations = repository.getmAllVacations();
+        final VacationAdapter vacationAdapter = new VacationAdapter(this);
+        recyclerView.setAdapter(vacationAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        vacationAdapter.setVacations(allVacations);
     }
 
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_excursion_list, menu);
+        getMenuInflater().inflate(R.menu.menu_vacation_list, menu);
         return true;
     }
 
